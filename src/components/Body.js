13bed87892afd68d54c1d4d020c07380d6b64e98
jilla pavan <jillapavan22001@gmail.com>
2024-03-19 15:10:32 +0530

    last commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,31 +2,32 @@ import React, { useState } from "react";
 
 function Body() {
   const [text, setText] = useState("");
-  const [click, setClick] = useState(false);
+  const [text1, setText1] = useState("");
 
   return (
     <div
-      className="w-96 h-96 m-auto border border-black flex items-center justify-center flex-col rounded-lg "
-      style={{ backgroundColor: text }}
+      className="w-96 h-96 mx-auto my-16 border border-black flex items-center justify-center flex-col rounded-lg shadow-lg"
+      style={{ backgroundColor: text  }}
     >
-      <input
-        value={text}
-        onChange={(e) => {
-          click && setText(e.target.value);
-        }}
-        className="border border-black rounded-lg p-2 w-8/12"
-        placeholder="Enter a color"
-        type="text"
-      />
-      <button
-        className="m-4 border border-black rounded-lg p-2"
-        onClick={() => {
-          setClick(true);
-          setText(text);
-        }}
-      >
-        Add Color To Background
-      </button>
+      <div className="flex items-center justify-center flex-col">
+        <input
+          value={text1}
+          onChange={(e) => {
+            setText1(e.target.value);
+          }}
+          className="border border-black rounded-lg p-2 w-full shadow-lg"
+          placeholder="Enter color"
+          type="text"
+        />
+        <button
+          onClick={() => {
+            setText(text1);
+          }}
+          className="m-4 p-2 border border-black rounded-lg bg-slate-100 hover:bg-slate-200  font-bold shadow-lg"
+        >
+          Add backgroundColor
+        </button>
+      </div>
     </div>
   );
 }
